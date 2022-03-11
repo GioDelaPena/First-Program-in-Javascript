8fa0910b5debf1a5cc6bf2ae9737dc41c88956a7
GioDelaPena <90368122+GioDelaPena@users.noreply.github.com>
2022-03-11 13:32:29 +0800

    Gio O. Dela Peña  BSIT 2-C

---
+++ console.js
@@ -0,0 +1,34 @@
+// ===============* My Peronal Information *===============
+
+alert('Interactive Programming: First Program in JavaScript')
+
+// Declaring a variables
+let age = 19;
+let contact = 9635839945;
+let school = 'Eastern Visayas State University Carigara Campus (EVSUCC)';
+let course = 'Bachelor of Science in Information Technology (BSIT)';
+let year = 2;
+let section = 'C'
+
+// Declaring a variables inside an object
+const me = {
+    fName: 'Gio',
+    mName: 'Oledan',
+    lName: 'Dela Peña',
+    address: 'Brgy. parag-um Carigara, Leyte',
+    unit: 'Block 1, Lot 23 vergabriel Heights Subdivision',
+}
+
+
+// Printing
+console.log('First Name:', me.fName);
+console.log('Middle Name:', me.mName);
+console.log('Last Name:', me.lName);
+console.log('Age:', age);
+console.log('Contact No.:',contact);
+console.log('Address:', me.address);
+console.log('Unit:', me.unit);
+console.log('School:', school);
+console.log('Course:', course);
+console.log('Year:', year);
+console.log('Section:', section);
